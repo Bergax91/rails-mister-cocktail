0a78ed2e07c6288da392186eae26b98886f6f264
Axel Norberg <a.norberg91@gmail.com>
2019-11-07 15:54:24 +0100

    exo2 controller ok

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,46 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+   def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @dose.cocktail = @dose
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:ingredient_id, :cocktail_id, :description)
+  end
+end
+
+
+# def create
+#     @review = Review.new(review_params)
+#     # we need `restaurant_id` to associate review with corresponding restaurant
+#     @restaurant = Restaurant.find(params[:restaurant_id])
+#     @review.restaurant = @restaurant
+#     @review.save
+#     redirect_to restaurant_path(@restaurant)
+#   end
+
+#   private
+
+#   def review_params
+#     params.require(:review).permit(:content)
+#   end
+# end
