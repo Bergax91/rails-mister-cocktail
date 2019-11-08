e428fb3c385898f38f8b23da15c422ff7b1fb627
Axel Norberg <a.norberg91@gmail.com>
2019-11-08 09:31:24 +0100

    exo2ok

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,7 +8,7 @@ class DosesController < ApplicationController
    def create
     @dose = Dose.new(dose_params)
     @cocktail = Cocktail.find(params[:cocktail_id])
-    # @dose.cocktail = @dose
+    @dose.cocktail = @cocktail
     if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
@@ -24,7 +24,7 @@ class DosesController < ApplicationController
   private
 
   def dose_params
-    params.require(:dose).permit(:ingredient_id, :cocktail_id, :description)
+    params.require(:dose).permit(:ingredient_id, :description)
   end
 end
 
